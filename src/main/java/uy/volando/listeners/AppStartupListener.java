7b8049d6450150699cb4bfb9a7fe38550c555b77
goncho09 <gonzalolarrica@gmail.com>
2025-11-20 17:03:41 -0300

    fix

---
+++ src/main/java/uy/volando/listeners/AppStartupListener.java
@@ -0,0 +1,35 @@
+package uy.volando.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.List;
+
+import uy.volando.soap.ControladorWS;
+import uy.volando.soap.WSConfig;
+import uy.volando.soap.client.DtAerolinea;
+import uy.volando.soap.client.VolandoServicePort;
+
+@WebListener
+public class AppStartupListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            WSConfig.init(sce.getServletContext());
+
+            VolandoServicePort ws = ControladorWS.getPort();
+
+            sce.getServletContext().setAttribute("categorias", ws.listarCategorias());
+
+        } catch (Exception e) {
+            System.err.println("¡Error al cargar los datos iniciales!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
